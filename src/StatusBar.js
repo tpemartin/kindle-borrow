0badc3d2642d1578e0bfb5bca1e049e38fae3d56
Martin老師 <tpemartin@gmail.com>
2022-11-20 07:52:59 +0800

    State dependent return

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -1,12 +1,23 @@
 function StatusBar(){
-
+  
+    if(onHold){
+        return <div className="status-bar">
+            <BorrowButton></BorrowButton><PutOnHold onHold={onHold}></PutOnHold>
+        </div>
+    } else {
+        return <div>
+            <PutOnHold onHold={onHold}></PutOnHold>
+        </div>
+    }
 
 }
 
 function BorrowButton(){
-
+    return <div id="borrow-btn"></div>
 }
 
-function PutOnHold(){
-    
+function PutOnHold({onHold}){
+   return <div id="put-on-hold"> 
+   {/* need more design */}
+   </div>
 }
