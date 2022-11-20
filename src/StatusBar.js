da6fb4d206faf63a9417a80cdd5bf1e4ff414359
Martin老師 <tpemartin@gmail.com>
2022-11-20 09:48:21 +0800

    complete status bar example

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -1,23 +1,38 @@
-function StatusBar(){
-  
-    if(onHold){
-        return <div className="status-bar">
-            <BorrowButton></BorrowButton><PutOnHold onHold={onHold}></PutOnHold>
-        </div>
-    } else {
-        return <div>
-            <PutOnHold onHold={onHold}></PutOnHold>
-        </div>
+import { Button, FormGroup, Switch, FormControlLabel } from "@mui/material"
+import { useState } from "react"
+
+export default function StatusBar({initialOnHold}){
+    var [onHold, setOnHold]=useState(initialOnHold)
+
+    //call back that uses setState function has to have the right scope (視野) to know the existence
+    // of setState.
+    function changeOnHoldState(){
+        setOnHold(!onHold)
     }
+
+    return <StatusBarContent onHold={onHold} onChange={changeOnHoldState}></StatusBarContent>
+}
+
+function StatusBarContent({onHold, onChange}){
+
+    return <div className="status-bar">
+        <PutOnHold onHold={onHold} onChange={onChange}></PutOnHold>
+        
+        {onHold?<BorrowButton></BorrowButton>:<></>}
+    </div>
+
 
 }
 
 function BorrowButton(){
-    return <div id="borrow-btn"></div>
+    return <Button variant="contained" id="borrow-btn">領取</Button>
 }
 
-function PutOnHold({onHold}){
-   return <div id="put-on-hold"> 
-   {/* need more design */}
-   </div>
+function PutOnHold({onHold, onChange}){
+
+    return <FormGroup>
+    <FormControlLabel 
+    labelPlacement="top"
+    control={<Switch defaultChecked={onHold} onChange={onChange} />} label="預約"  />
+    </FormGroup>
 }
